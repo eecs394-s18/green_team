beb3d88f46c4f919d96547ddeb129a6136030475
morgan979 <morgan979@gmail.com>
2018-04-10 16:27:47 -0500

    added profiles to firebase

--- MyIonicProject/src/pages/profiles/profiles.ts
+++ MyIonicProject/src/pages/profiles/profiles.ts
@@ -16,11 +16,44 @@ import * as firebase from 'Firebase';
 })
 export class ProfilesPage {
 
+  // profiles = [];
+  // data = { profile:'' };
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	let ref = firebase.database().ref('profiles/').push();
+    ref.set({
+      profile:"test"
+    });
+    
+  // 	this.ref.on('value', resp => {
+  //   this.profiles = [];
+  //   this.profiles = snapshotToArray(resp);
+  // });
+
   }
+
+  // addProfile() {
+  //   let newData = this.ref.push();
+  //   newData.set({
+  //     email:this.data.email
+  //     });
+  // this.navCtrl.pop();
+  // }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilesPage');
   }
 
 }
+
+// export const snapshotToArray = snapshot => {
+//     let returnArr = [];
+
+//     snapshot.forEach(childSnapshot => {
+//         let item = childSnapshot.val();
+//         item.key = childSnapshot.key;
+//         returnArr.push(item);
+//     });
+
+//     return returnArr;
+// };
