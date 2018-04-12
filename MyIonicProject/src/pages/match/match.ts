5159bcc323c5018fc32ac5b75a26ed3652b144c0
William Ho <awho168@gmail.com>
2018-04-11 23:38:14 -0500

    match ui and display of data from firebase

--- MyIonicProject/src/pages/match/match.ts
+++ MyIonicProject/src/pages/match/match.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams, Content } from 'ionic-angular';
+import * as firebase from 'Firebase';
 
 @IonicPage()
 @Component({
@@ -9,4 +10,28 @@ import { NavController, NavParams, Content } from 'ionic-angular';
 
 export class MatchPage {
 
+  public allUsers;
+  public chosenUsers;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.allUsers = {};
+    this.chosenUsers = [];
+  }
+
+  ionViewDidLoad() {
+    const ref = firebase.database().ref('users');
+    const users = ref.on('value', snapshot => {
+      this.allUsers = snapshot.val();
+      this.selectUsers();
+    });
+  }
+
+  selectUsers() {
+    // TODO: Algorithm for matching users goes here.
+
+    let keys:string[] = Object.keys(this.allUsers);
+    for (let i:number = 0; i < 4; i++) {
+      this.chosenUsers.push(this.allUsers[keys[i]]);
+    }
+  }
 }
