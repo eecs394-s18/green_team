ff18bcfed32f891e264281cd21b3ca56bdc80ab1
Michael Hsu <michael100.hsu@gmail.com>
2018-04-14 10:57:29 +0800

    adding chatting to existing chats

--- MyIonicProject/src/pages/chats/chats.ts
+++ MyIonicProject/src/pages/chats/chats.ts
@@ -21,6 +21,7 @@ export class ChatsPage {
   public chosenUsers;
   public existingChats;
   private loading;
+  private nameToUUID;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController) {
     this.allUsers = {};
@@ -67,47 +68,12 @@ export class ChatsPage {
     });
   }
 
-  // chatUser(user): void {
-  //
-  //   const rooms = firebase.database().ref('chatrooms');
-  //   const currUser = firebase.auth().currentUser;
-  //   const email_set = new Set([currUser.email,user.email]);
-  //   var key = '', nickname = currUser.displayName, otherNickname = user.username, roomFound = false;
-  //   const chatID = objectHash(email_set);
-  //
-  //   //check if in ddb
-  //   // theres some weird bug here but it seems to work anyways
-  //   var new_chat = false;
-  //   rooms.once('value')
-  //   .then(function(snap){
-  //     if(snap.child(chatID).exists()){
-  //       console.log('chatroom already exists')
-  //       console.log(user)
-  //       new_chat = false;
-  //     }
-  //     else{
-  //       new_chat=true;
-  //       console.log('chatroom does not exist')
-  //     }
-  //   })
-  //   //its not in db
-  //   // this will probably screw up the chat history
-  //   if(new_chat){
-  //     var tmp = {}
-  //     tmp[chatID] = {}
-  //     tmp[chatID]['members'] = {}
-  //     tmp[chatID]['members'][nickname] = true;
-  //     tmp[chatID]['members'][otherNickname] = true;
-  //
-  //     rooms.update(tmp)
-  //   }
-  //
-  //
-  //   this.navCtrl.setRoot(ChatPage, {
-  //     key: chatID,
-  //     nickname: nickname,
-  //     otherNickname: otherNickname
-  //   });
-  // }
+  chatUser(user): void {
+    this.navCtrl.setRoot(ChatPage, {
+      key: this.nameToUUID[user.username],
+      nickname: firebase.auth().currentUser.displayName,
+      otherNickname: user.username
+    });
+  }
 
 }
