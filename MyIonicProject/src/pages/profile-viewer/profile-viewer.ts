da9affa55bf3c120c394cb63bef18e48e6cba8f9
alex-morikado <asmorikado+git@gmail.com>
2018-04-26 11:43:43 -0500

    update profile view formatting

--- MyIonicProject/src/pages/profile-viewer/profile-viewer.ts
+++ MyIonicProject/src/pages/profile-viewer/profile-viewer.ts
@@ -28,22 +28,46 @@ export class ProfileViewerPage {
               public navParams: NavParams,
               private storageProvider: StorageProvider) {
 
-      this.person = {
-          username: undefined,
-          country: undefined,
-          languages: undefined,
-      };
+      this.person = {};
       this.prof_pic = 'avatar.png';
       this.user = firebase.database().ref('/users/' + navParams.get('uid'));
   }
-
-
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfileViewerPage');
     this.user.once('value', snapshot => {
         this.person = snapshot.val();
-        console.log(this.person)
+        console.log(this.person);
+        let movies = "";
+        const num_movies = Object.keys(this.person.movies).length;
+        for (let i:number = 0; i < num_movies; i++){
+            movies += this.person.movies[i]
+            if (i < num_movies - 1){
+                movies += ', ';
+            }
+        }
+        this.person.movies = movies;
+
+        let music = "";
+        const num_music = Object.keys(this.person.music).length;
+        for (let i:number = 0; i < num_music; i++){
+            music += this.person.music[i]
+            if (i < num_music - 1){
+                music += ', ';
+            }
+        }
+        this.person.music = music;
+
+        let sports = "";
+        const num_sports = Object.keys(this.person.sports).length;
+        for (let i:number = 0; i < num_sports; i++){
+            sports += this.person.sports[i]
+            if (i < num_sports - 1){
+                sports += ', ';
+            }
+        }
+        this.person.sports = sports;
+
         //replace avatar.png with the appropriate filename once picture uploads are complete
         this.storageProvider.getPictureURL(this.prof_pic).then(url => this.prof_pic = url);
     });
